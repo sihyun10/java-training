aced32975846b273227de7e1cf7e6d3165096410
sihyun10 <hyhy200170@gmail.com>
2024-01-27 01:26:35 +0900

    Feat: 계산기의 계산기능 메서드 추출 계산기의 계산기능 중 더하기, 빼기, 곱하기 기능을 메서드로 추출하여 main()코드에서 계산기능을 구현하는게 아닌, 메서드를 불러와 계산기능을 사용할 수 있게 되었다.

---
+++ src/training/calculator/CalculatorMethod.java
@@ -0,0 +1,72 @@
+package training.calculator;
+
+import java.util.Scanner;
+
+public class CalculatorMethod {
+    public static void main(String[] args) {
+        int result = 0;
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("| 1.계산기능 | 2.총 결과 | 3.초기화 | 4.종료 |");
+            System.out.print("항목 선택: ");
+            int choice = input.nextInt();
+            input.nextLine();
+
+            int num;
+            if (choice == 1) {
+                System.out.print("계산할 연산자를 입력해주세요: ");
+                String operator = input.nextLine();
+
+                if (operator.equals("+")) {
+                    System.out.print("계산할 숫자를 입력해주세요: ");
+                    num = input.nextInt();
+                    result = add(result, num);
+                } else if (operator.equals("-")) {
+                    System.out.print("계산할 숫자를 입력해주세요: ");
+                    num = input.nextInt();
+                    result = sub(result, num);
+                } else if (operator.equals("*")) {
+                    System.out.print("계산할 숫자를 입력해주세요: ");
+                    num = input.nextInt();
+                    result = mul(result, num);
+                } else {
+                    System.out.println("잘못된 연산자입니다. 다시 시도해주세요.");
+                }
+            } else if (choice == 2) {
+                System.out.println("총 값의 결과는 " + result);
+            } else if (choice == 3) {
+                result = 0;
+                System.out.println("값이 모두 초기화 되었습니다.");
+                System.out.println("다시 계산을 시작하려면 1, 종료하려면 4를 선택해주세요.");
+            } else if (choice == 4) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("항목이 없는 선택입니다. 다시 선택해주세요.");
+            }
+        }
+    }
+
+    public static int add(int result, int num) {
+        result += num;
+        System.out.println("숫자 " + num + "을 더하셨습니다. 현재 값의 결과는 " + result);
+        return result;
+    }
+
+    public static int sub(int result, int num) {
+        if (num < result) {
+            result -= num;
+            System.out.println("숫자 " + num + "을 빼셨습니다. 현재 값의 결과는 " + result);
+        } else {
+            System.out.println("현재 값의 결과보다 큰 값입니다. 다시 시도해주세요");
+        }
+        return result;
+    }
+
+    public static int mul(int result, int num) {
+        result *= num;
+        System.out.println("숫자 " + num + "을 곱하셨습니다. 현재 값의 결과는 " + result);
+        return result;
+    }
+}
