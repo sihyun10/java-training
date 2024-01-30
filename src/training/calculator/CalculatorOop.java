52944960edea3edf135a2043d9df8e29b01bf5ee
sihyun10 <hyhy200170@gmail.com>
2024-01-30 16:13:51 +0900

    Refactor: 객체 데이터 숨기고, 외부에서 직접 접근 불가능하게 수정 이전의 코드는 외부에서 직접 접근이 가능해 CalculatorOoop클래스 안에서 powerOn데이터를 다루는 모든 로직을 무시하고, 변경이 가능하였습니다. 그렇기때문에 멤버변수(필드)를 private로 작성해주었고, 외부 접근을 막았습니다. 또한, add(), subtract() .. 의 기능이 계산기가 켜져있든 꺼져있든 상관없이 계산이 되었는데, 계산기의 켜져있음과 꺼져있음을 확인하는 메서드를 작성해, 확인 후 계산할 수 있도록 해주었습니다.

--- src/training/calculator/CalculatorOop.java
+++ src/training/calculator/CalculatorOop.java
@@ -1,8 +1,8 @@
 package training.calculator;
 
 public class CalculatorOop {
-    int currentValue; //현재값
-    boolean powerOn; //전원상태 (켜짐/꺼짐)
+    private int currentValue; //현재값
+    private boolean powerOn; //전원버튼
 
     //전원 켜기,끄기 기능
     void turnOn() {
@@ -17,22 +17,32 @@ public class CalculatorOop {
 
     //계산기능
     void add(int calculateValue) {
+        checkPowerStatus();
         currentValue += calculateValue;
         System.out.println(currentValue);
     }
 
     void subtract(int calculateValue) {
+        checkPowerStatus();
         currentValue -= calculateValue;
         System.out.println(currentValue);
     }
 
     void multiply(int calculateValue) {
+        checkPowerStatus();
         currentValue *= calculateValue;
         System.out.println(currentValue);
     }
 
     //현재값 출력기능
     void currentValuePrint() {
+        checkPowerStatus();
         System.out.println("총 계산 결과는: " + currentValue);
+    }
+
+    private void checkPowerStatus() {
+        if (!powerOn) {
+            throw new RuntimeException("전원이 꺼져있습니다. 계산을 할 수 없습니다.");
+        }
     }
 }
