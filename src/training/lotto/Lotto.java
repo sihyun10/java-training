43c5b6d74f80ce32dc94155dc796d073041e6a75
sihyun10 <hyhy200170@gmail.com>
2024-05-08 23:29:20 +0900

    Refactor: 메서드 기능 쪼개기 원래 extract() 메서드 안에는 세가지 기능을 다 한꺼번에 가지고 있었다. 바로 '랜덤으로 번호 추출하는 기능', '중복인지 확인하는 기능', 'sort()해서 오름차순 하는 기능'이다. 따라서 각 메서드가 하나의 기능을 수행하도록 리팩토링하였다.

--- src/training/lotto/Lotto.java
+++ src/training/lotto/Lotto.java
@@ -13,24 +13,31 @@ public class Lotto {
         count = 0;
 
         while (count < 6) {
-            int num = random.nextInt(45) + 1;
-            boolean isDuplicate = false;
-
-            for (int i = 0; i < count; i++) {
-                if (lottoNumbers[i] == num) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-
-            if (!isDuplicate) {
+            int num = extractNumber();
+            if (!isDuplicate(num)) {
                 lottoNumbers[count] = num;
                 count++;
             }
         }
 
-        Arrays.sort(lottoNumbers);
-
+        sortNumbers();
         return lottoNumbers;
+    }
+
+    private int extractNumber() {
+        return random.nextInt(45) + 1;
+    }
+
+    private boolean isDuplicate(int num) {
+        for (int i = 0; i < count; i++) {
+            if (lottoNumbers[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void sortNumbers() {
+        Arrays.sort(lottoNumbers);
     }
 }
