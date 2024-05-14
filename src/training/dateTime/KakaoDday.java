c0773a8cfa350f7f5b78be877aa2a1f9202e3c03
sihyun10 <hyhy200170@gmail.com>
2024-05-14 13:42:59 +0900

    설정일로부터 1일 포함여부에 따른 코드 수정 디데이를 설정하는 날짜를 1일 포함한다고 했을때, 디데이 값이 달라지게 된다. 따라서 그에 맞게 코드를 수정하였다.

--- src/training/dateTime/KakaoDday.java
+++ src/training/dateTime/KakaoDday.java
@@ -11,31 +11,45 @@ public class KakaoDday {
 
         System.out.print("설정하고자 하는 날짜를 입력해주세요[2024.05.14]: ");
         String date = scanner.nextLine();
+        System.out.print("설정 날짜를 1일로 포함하겠습니까?[네/아니오]: ");
+        String include = scanner.nextLine();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        LocalDate localDate = LocalDate.parse(date, formatter);
+        LocalDate setDate = LocalDate.parse(date, formatter);
         LocalDate nowDate = LocalDate.now();
 
-        if (nowDate.isBefore(localDate)) {
-            comingDday(localDate, nowDate);
-        } else if (nowDate.isAfter(localDate)) {
-            passDday(localDate, nowDate);
-        } else if (nowDate.isEqual(localDate)) {
-            System.out.println("D-DAY");
+        if (nowDate.isBefore(setDate)) {
+            comingDday(setDate, nowDate);
+        } else if (nowDate.isAfter(setDate)) {
+            passDday(setDate, nowDate, include);
+        } else if (nowDate.isEqual(setDate)) {
+            nowDday(setDate, include);
         }
     }
 
     //다가오는 디데이 기능
-    private static void comingDday(LocalDate date, LocalDate nowDate) {
-        LocalDate endDate = LocalDate.of(date.getYear(), date.getMonth(), date.getDayOfMonth());
-        long daysBetween = ChronoUnit.DAYS.between(nowDate, endDate);
+    private static void comingDday(LocalDate setDate, LocalDate nowDate) {
+        long daysBetween = ChronoUnit.DAYS.between(nowDate, setDate);
         System.out.println("D-" + daysBetween);
     }
 
     //시간이 흐른 디데이 기능
-    private static void passDday(LocalDate date, LocalDate nowDate) {
-        LocalDate startDate = LocalDate.of(date.getYear(), date.getMonth(), date.getDayOfMonth());
-        long daysBetween = ChronoUnit.DAYS.between(startDate, nowDate);
+    private static void passDday(LocalDate setDate, LocalDate nowDate, String include) {
+        long daysBetween = 0;
+        if (include.equals("네")) {
+            daysBetween = ChronoUnit.DAYS.between(setDate, nowDate) + 1;
+        } else if (include.equals("아니오")) {
+            daysBetween = ChronoUnit.DAYS.between(setDate, nowDate);
+        }
+
         System.out.println("D+" + daysBetween);
+    }
+
+    private static void nowDday(LocalDate setDate, String include) {
+        if (include.equals("네")) {
+            System.out.println("D+1");
+        } else {
+            System.out.println("D-DAY");
+        }
     }
 }
